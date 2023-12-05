ebcc153fc13042e312ba79c1c51eeac9f564891d
alnabid <alnabidrm@gmail.com>
2023-12-05 23:38:06 +0600

    Commit

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,17 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true,
 });
+
+const seeMore = document.getElementById('seeMore')
+const seeLess = document.getElementById('seeLess')
+const moreText = document.getElementById('moreText')
+moreText.style.display = 'none'
+
+seeMore.addEventListener('click', (e) => {
+    moreText.style.display = 'block'
+    e.target.style.display = 'none'
+})
+seeLess.addEventListener('click', (e) => {
+    moreText.style.display = 'none'
+    seeMore.style.display = 'block'
+})
