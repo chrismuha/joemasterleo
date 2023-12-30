0d3ada611050325918b443edbe89da96d28d1ffd
alnabid <alnabidrm@gmail.com>
2023-12-31 02:11:12 +0600

    Commit : added gif as header image

--- public/js/main.js
+++ public/js/main.js
@@ -19,12 +19,3 @@ seeLess.addEventListener('click', (e) => {
     moreText.style.display = 'none'
     seeMore.style.display = 'block'
 })
-
-const body = document.getElementsByTagName('body');
-body.addEventListener('hover', (e) => {
-    playVideo();
-})
-function playVideo() {
-    let video = document.getElementById("myVideo");
-    video.play();
-}
