fdba440598da2dea9ed5a77871975e302b7dc214
alnabid <alnabidrm@gmail.com>
2024-01-10 20:01:12 +0600

    Commit : read more feature : main.js modification

--- public/js/main.js
+++ public/js/main.js
@@ -6,22 +6,35 @@ var swiper = new Swiper(".mySwiper", {
     loop: true,
 });
 
-const seeMore = document.getElementById('seeMore')
-const seeLess = document.getElementById('seeLess')
-const moreText = document.getElementById('moreText')
-moreText.style.display = 'none'
+const seeMore = Array.from(document.querySelectorAll('.seeMore'));
+const seeLess = Array.from(document.querySelectorAll('.seeLess'));
+const moreText = Array.from(document.querySelectorAll('.moreText'));
 
-seeMore.addEventListener('click', (e) => {
-    moreText.style.display = 'block'
-    e.target.style.display = 'none'
-})
-seeLess.addEventListener('click', (e) => {
-    moreText.style.display = 'none'
-    seeMore.style.display = 'block'
-})
+moreText.forEach(e => {
+    e.style.display = 'none';
+});
 
-var elements = document.querySelectorAll('.simple-bar');
+seeMore.forEach(e => {
+    e.addEventListener('click', (e) => {
+        const parent = e.target.closest('.has-read-more-feature');
+        const moreText = parent.querySelector('.moreText');
+        moreText.style.display = 'block';
+        e.target.style.display = 'none';
+    });
+});
 
-elements.forEach(function (element) {
-    new SimpleBar(element, { autoHide: false });
+seeLess.forEach(e => {
+    e.addEventListener('click', (e) => {
+        const parent = e.target.closest('.has-read-more-feature'); 
+        const moreText = parent.querySelector('.moreText');
+        moreText.style.display = 'none';
+        const seeMore = parent.querySelector('.seeMore');
+        seeMore.style.display = 'block';
+    });
+});
+
+let simpleBar = document.querySelectorAll('.simple-bar');
+
+simpleBar.forEach(function (e) {
+    new SimpleBar(e, { autoHide: false });
 });
